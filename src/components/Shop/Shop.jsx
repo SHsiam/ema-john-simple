a9c521b2b82df6e58718a456a7357b42d2b66f76
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-03-31 01:21:51 +0600

    add cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -15,13 +15,37 @@ const Shop = () => {
 
     useEffect(()=>{
         const storeCard=getShoppingCart();
+        const saveCart=[];
         //step1: get id
         for(const id in storeCard){
-
+            //step2: get the product by using id
+            const addedProduct=products.find(product=> product.id===id);
+            if(addedProduct){
+            //step3: get quantity of the product
+            const quantity=storeCard[id];
+            addedProduct.quantity=quantity;
+            //step4: add the added product to the save cart
+            saveCart.push(addedProduct);
+            }
         }
+        //set5: set the cart
+        setCart(saveCart);
     },[products])
     const handleAddToCart = (product) => {
-       const newCart=[...cart,product];
+        // let newCart=[];
+    const newCart=[...cart,product];
+    //if product doesn't exist in the cart then set quantity= 1;
+    //if exist update quantity by 1
+    // const exists = cart.find(pd => pd.id === product.id);
+    //     if(!exists){
+    //         product.quantity = 1;
+    //         newCart= [...cart, product]
+    //     }
+    //     else{
+    //         exists.quantity = exists.quantity + 1;
+    //         const remaining = cart.filter(pd => pd.id !== product.id);
+    //         newCart = [...remaining, exists];
+    //     }
        setCart(newCart);
        addToDb(product.id);
     }
